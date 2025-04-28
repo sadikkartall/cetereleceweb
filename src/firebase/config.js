84ba81aa7789c0c33be19c4a96ddd05ee6c807a9
sadikkartall <sdkkrtll@gmail.com>
2025-04-28 11:16:58 +0300

    Hassas bilgileri ortam değişkenlerine taşı ve .env.example dosyası ekle

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,15 +4,15 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getAnalytics } from 'firebase/analytics';
 
-// Firebase yapılandırması
+// Firebase yapılandırması - çevre değişkenlerinden alınır
 const firebaseConfig = {
-  apiKey: "AIzaSyD5h-Ezu83f1KqhVhUjJMhClTXBdBLup20",
-  authDomain: "ceterelecenet.firebaseapp.com",
-  projectId: "ceterelecenet",
-  storageBucket: "ceterelecenet.appspot.com",
-  messagingSenderId: "424785111488",
-  appId: "1:424785111488:web:c997563cefd7b65918c025",
-  measurementId: "G-ZFWF6EV9JD"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Firebase'i başlat
